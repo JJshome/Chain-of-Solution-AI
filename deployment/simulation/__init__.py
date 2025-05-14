d65224cf7769d1dde54c09caba48283a13f12cef
JJshome <107289883+JJshome@users.noreply.github.com>
2025-05-15 01:44:37 +0900

    Add simulation package initialization file

---
+++ deployment/simulation/__init__.py
@@ -0,0 +1,8 @@
+"""
+Simulation environment for the Chain of Solution framework.
+
+This module provides simulation tools for testing the Chain of Solution framework
+with synthetic data and visualizing the results.
+"""
+
+from .cos_simulation import CoSSimulation
